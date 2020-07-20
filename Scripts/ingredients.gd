5f21d41b121b981c5033e75bd6569c78dcbf4c72
baetup <51537834+baetup@users.noreply.github.com>
2020-07-20 16:36:26 +0200

    updated farm

--- Scripts/ingredients.gd
+++ Scripts/ingredients.gd
@@ -1,8 +1,15 @@
 extends Node
 
 
-var flour = ingredient.new("res://Image-assets/flourIcon.png", "Flour", 2, 1, 5 )
-var sugar = ingredient.new("res://Image-assets/flourIcon.png", "Sugar", 3, 1, 5 )
+var farmers = 0
+var fishers = 0
+var hunters = 0
+var farmersLife = 5.00
+var fishersLife = 5.00
+var huntersLife = 5.00
+
+var flour = ingredient.new("res://Image-assets/flourIcon.png", "Flour", 2, 1, 5, 1, 5)
+var sugar = ingredient.new("res://Image-assets/flourIcon.png", "Sugar", 3, 1, 5, 1, 10)
 
 class ingredient :
 	var icon
@@ -10,13 +17,17 @@ class ingredient :
 	var quantity
 	var produceTime
 	var produceAmount
+	var level
+	var levelCost
 
-	func _init(setIcon : String, setName : String, setCount : int, setProduceTime : float, setProduceAmount : float):
+	func _init(setIcon : String, setName : String, setCount : int, setProduceTime : float, setProduceAmount : float,setLevel:int, setLevelCost :int):
 		icon = setIcon
 		name = setName
 		quantity = setCount
 		produceTime = setProduceTime
 		produceAmount = setProduceAmount
+		level = setLevel
+		levelCost = setLevelCost
 		
 	func addQuantity(amount : int):
 		quantity = quantity + amount
@@ -28,4 +39,8 @@ class ingredient :
 		produceTime = produceTime - (produceTime * 0.02)
 		
 	func setProduceAmount(amount):
-		produceAmount = amount
+		produceAmount += amount
+	
+	func setLevelCost(tinyFloat : float):
+		levelCost = levelCost + (levelCost * tinyFloat )
+
